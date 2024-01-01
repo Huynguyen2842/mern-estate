af208bcfb7b06535fcd06e6bc0a9a422d96a241b
huynguyen2842 <gianghuy2804@gmail.com>
2024-01-01 17:55:46 +0700

    create a midleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -1,24 +1,25 @@
 // Importing required modules
-import express from 'express'; // Express framework for building web applications
-import mongoose from 'mongoose'; // Mongoose for MongoDB database connection
-import dotenv from 'dotenv'; // dotenv for loading environment variables
-import userRouter from './routes/user.route.js'; // Custom user router
-import authRouter from './routes/auth.route.js'; // Custom auth router
+import express from "express"; // Express framework for building web applications
+import mongoose from "mongoose"; // Mongoose for MongoDB database connection
+import dotenv from "dotenv"; // dotenv for loading environment variables
+import userRouter from "./routes/user.route.js"; // Custom user router
+import authRouter from "./routes/auth.route.js"; // Custom auth router
 dotenv.config(); // Loading environment variables from .env file
 
 const app = express(); // Creating an instance of the Express application
 
 // Connecting to MongoDB database
-mongoose.connect(process.env.MONGO)
+mongoose
+  .connect(process.env.MONGO)
   .then(() => {
-    console.log('Connected to MongoDB!!');
+    console.log("Connected to MongoDB!!");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!!');
+  console.log("Server is running on port 3000!!");
 });
 
 app.use(express.json()); // Using express.json() middleware for parsing JSON bodies of requests
@@ -26,3 +27,13 @@ app.use(express.json()); // Using express.json() middleware for parsing JSON bod
 // Using the custom user router for handling user-related routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
